595bf4b7996019a4c7a681d9029a90b4c12d4ee5
neepo <neepo@phantomtech.io>
2026-05-06 17:30:40 +0100

    Smaller Merge Fixes

--- packages/sdk/js/src/v2/gen/sdk.gen.ts
+++ packages/sdk/js/src/v2/gen/sdk.gen.ts
@@ -2522,7 +2522,7 @@ export class Permission extends HeyApiClient {
       requestID: string
       directory?: string
       workspace?: string
-      reply?: "once" | "always" | "reject"
+      reply?: "once" | "always" | "reject" | "manual_apply"
       message?: string
     },
     options?: Options<never, ThrowOnError>,
@@ -2566,7 +2566,7 @@ export class Permission extends HeyApiClient {
       permissionID: string
       directory?: string
       workspace?: string
-      response?: "once" | "always" | "reject"
+      response?: "once" | "always" | "reject" | "manual_apply"
     },
     options?: Options<never, ThrowOnError>,
   ) {
